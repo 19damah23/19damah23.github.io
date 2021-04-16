dbc405ce2e8276c5f276003a4a6fb03f0123a8ed
Muchamad Agus Hermawan <muchamadagush@gmail.com>
2021-04-17 03:12:18 +0700

    make the navbar fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,11 @@ const Navbar = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   return (
-    <div>
-      <div className="flex justify-between flex-col lg:flex-row">
+    <div className="fixed w-full">
+      <div className="flex justify-between flex-col lg:flex-row px-16 py-4">
         <div className="flex justify-between items-center px-4 py-4 lg:py-0 border-b lg:border-b-0">
           <div>
-            <a href="/" className="font-semibold uppercase text-white">
+            <a href="/" className="font-light uppercase text-white text-5xl">
               agus
             </a>
           </div>
@@ -43,29 +43,29 @@ const Navbar = () => {
           </div>
         </div>
         <div
-          className={`${isMobile ? "block" : "hidden"
+          className={`${isMobile ? "block bg-black bg-opacity-60" : "hidden"
             } lg:flex flex-col lg:flex-row py-4 lg:py-0`}
         >
           <a
-            href="#"
+            href="#about"
             className="block uppercase px-4 py-3 lg:py-5 text-white hover:text-gray-500"
           >
             about
           </a>
           <a
-            href="#"
+            href="#experience"
             className="block uppercase px-4 py-3 lg:py-5 text-white hover:text-gray-500"
           >
             experience
           </a>
           <a
-            href="#"
+            href="#works"
             className="block uppercase px-4 py-3 lg:py-5 text-white hover:text-gray-500"
           >
             works
           </a>
           <a
-            href="#"
+            href="#contact"
             className="block uppercase px-4 py-3 lg:py-5 text-white hover:text-gray-500"
           >
             contact
